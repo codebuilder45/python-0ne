abca3c08da76b34f789e4660ac1b87c1a9c86c64
codebuilder45 <sdonahoe@students.mchenry.edu>
2024-12-09 15:32:37 -0600

    addded new program

---
+++ area23.py
@@ -0,0 +1,9 @@
+# this program will take the length and width of a rectangle to calculate the area
+# and display the answer.
+length_rec = 4
+width_rec = 9
+area = length_rec * width_rec
+print("\tThe length is: ", "\t\b\b\b\b\b" + str(length_rec))
+print("\tThe width is: ", "\t   " + str(width_rec))
+print("\n\tThe area of the rectangle with a length of",
+      str(length_rec), "and a width of", str(width_rec), "is", str(area), end=".")
---
+++ zz.py
@@ -0,0 +1,4 @@
+# x = 1
+# a = complex(x)
+# print(a)
+print(2 ** 3 ** 2)
