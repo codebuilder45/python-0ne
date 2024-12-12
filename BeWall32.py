51fc81b4cbc8964c67a4bd7d18355132b9515840
codebuilder45 <sdonahoe@students.mchenry.edu>
2024-12-11 20:24:48 -0600

    new prgm

--- BeWall32.py
+++ BeWall32.py
@@ -21,24 +21,15 @@ while wall_bottles != 0:
     # first line
     if wall_bottles >= 2:
         print(f"{wall_bottles} bottles of beer on the wall")
-    if wall_bottles == 1:
-        print(f"{wall_bottles} bottle of beer on the wall")
-    # second line
-    if wall_bottles >= 2:
         print(f"{wall_bottles} bottles of beer")
-    if wall_bottles == 1:
-        print(f"{wall_bottles} bottle of beer")
-    # third line
-    if wall_bottles >= 2:
         print("Take one down, pass it around")
-    if wall_bottles == 1:
-        print("Take it down, pass it around")
-    # take a bottle
-    wall_bottles -= 1
-    # fourth line
-    if wall_bottles >= 2:
-        print(f"{wall_bottles} bottles of beer on the wall!\n")
-    elif wall_bottles == 1:
-        print(f"{wall_bottles} bottle of beer on the wall!\n")
-    elif wall_bottles == 0:
-        print("No bottles of beer on the wall!")
+        wall_bottles -= 1
+        if wall_bottles >= 2:
+            print(f"{wall_bottles} bottles of beer on the wall!\n")
+        if wall_bottles == 1:
+            print(f"{wall_bottles} bottle of beer on the wall!\n")
+            print(f"{wall_bottles} bottle of beer on the wall")
+            print(f"{wall_bottles} bottle of beer")
+            print("Take it down, pass it around")
+            wall_bottles -= 1
+            print("No bottles of beer on the wall!")
